3bb4dac8aaab01b929656247461812d8cf2b02a8
younesk93 <bkassemi.younes@gmail.com>
2019-08-29 20:04:05 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,31 @@
+import sys
+from flask import Flask, render_template, jsonify, redirect
+import pymongo
+import scrape_mars
+
+sys.setrecursionlimit(2000)
+app = Flask(__name__)
+
+
+client = pymongo.MongoClient()
+db = client.mars_db
+collection = db.mars_facts
+
+
+@app.route('/scrape')
+def scrape():
+    mars = scrape_mars.scrape()
+    print("\n\n\n")
+    db.mars_facts.insert_one(mars)
+    return "Some scrapped data"
+
+
+@app.route("/")
+def home():
+    mars = list(db.mars_facts.find())
+    print(mars)
+    return render_template("index.html", mars=mars)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
